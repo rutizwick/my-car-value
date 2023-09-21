ea4e82fd14f572e76de91691e94daf72f61ca6fd
ruti.zwick@khealth.com <ruti.zwick@khealth.com>
2023-09-21 11:33:37 +0300

    entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
+import { UserService } from './users/user.service';
 import { ReportsService } from './reports/reports.service';
 import { ReportsModule } from './reports/reports.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './users/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Report],
       synchronize: true,
     }),
   ],
