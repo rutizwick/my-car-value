3b890408bb5dbec3d5118767aba2bbed5c692fd3
ruti.zwick@khealth.com <ruti.zwick@khealth.com>
2023-09-26 11:34:09 +0300

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ReportsModule } from './reports/reports.module';
 import { UserModule } from './users/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
